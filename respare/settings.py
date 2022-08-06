c06c03397faaa4dce3239db91e5b5b10b0a0d157
Aravind <aravindackal@gmail.com>
2022-08-06 13:11:22 +0530

    first

--- respare/settings.py
+++ respare/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'rest_framework',
+    'rest_framework',
     'backend'
 ]
 
@@ -82,6 +82,10 @@ DATABASES = {
         'NAME': 'mydatabase',
     }
 }
+
+import dj_database_url
+db_from_env=dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
